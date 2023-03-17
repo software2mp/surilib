c2d332e74bd3b09ccf3ae9b58d4160616024f8fb
Javier Urien <javierurien@spacesur.com>
2023-03-17 20:41:49 +0100

    update copyright message

--- include/suri/GuiStateChangeMediator.h
+++ include/suri/GuiStateChangeMediator.h
@@ -1,12 +1,29 @@
-/** \file GuiStateChangeMediator.h */
-/*!
- *  Biblioteca de procesamiento GeoEspacial SuriLib.
- *  Copyright 2011-2007 CONAE - SUR Emprendimientos Tecnologicos S.R.L.
- *
- *  Este producto contiene software desarrollado por
- *  SUR Emprendimientos Tecnologicos S.R.L. (http://www.suremptec.com/)
- *
- */
+/* Copyright (c) 2006-2023 SpaceSUR and CONAE
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+This file is part of the SuriLib project and its derived programs. You must retain
+this copyright notice and license text in all copies or substantial
+portions of the software.
+
+If you develop a program based on this software, you must provide a visible
+notice to its users that it contains code from the SuriLib project and provide
+a copy of this license. The notice should be displayed in a way that is easily
+accessible to users, such as in the program's "About" box or documentation.
+
+For more information about SpaceSUR, visit <https://www.spacesur.com>.
+For more information about CONAE, visit <http://www.conae.gov.ar/>. */
 
 #ifndef GUISTATECHANGEMEDIATOR_H_
 #define GUISTATECHANGEMEDIATOR_H_
